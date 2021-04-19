44db6c53db22b8dac1d86cdcf008f0506a51e629
Greg Robinson <gregrob@mac.com>
2021-04-19 17:26:51 +1000

    More methods.

--- include/utils.h
+++ include/utils.h
@@ -2,10 +2,22 @@
 #define UTILS_H
 
 // Stringify argument
-#define STR_IMPL_(x)       (#x)
+#define STR_IMPL_(x)            (#x)
 
 // String length of a integer (null removed)
 // Indirect to expand argument macros
-#define STRNLEN_INT(x)      (sizeof(STR_IMPL_(x)) - 1)
+#define STRNLEN_INT(x)          (sizeof(STR_IMPL_(x)) - 1)
+
+// Convert a parameter from seconds to calls defined by the local call interval <MODULE_CALL_INTERVAL>
+#define SECS_TO_CALLS(seconds)  (seconds * (1000 / MODULE_CALL_INTERVAL))
+
+// Convert a parameter from calls defined by the local call interval <MODULE_CALL_INTERVAL> to seconds
+#define CALLS_TO_SECS(calls)    ((calls * MODULE_CALL_INTERVAL)/1000)
+
+// Enabled
+#define ENABLED                 0x1
+
+// Disabled
+#define DISABLED                0x0
 
 #endif
